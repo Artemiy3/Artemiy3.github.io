7b088f1daee2c7ba4f376fa31cf4f157b0da104b
Artemiy3 <71441105+Artemiy3@users.noreply.github.com>
2022-04-05 00:49:39 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,311 @@
+const ERC20_ADDRESS = "0x4F97818A8Ec49e9074DE0e80B46262C27b735f3b"
+const TRANSFER_FEE = "343000000000000"
+const ERC20_ABI = [
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "tokenOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokens",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokens",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "delegate",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getCoin",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "test",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numTokens",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "numTokens",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+async function connect(){
+    // window.addEventListener('load', () => {
+    // // Wait for loading completion to avoid race conditions with web3 injection timing.
+     if (window.ethereum) {
+       const web3 = new Web3(window.ethereum);
+       try {
+         // Request account access if needed
+         await window.ethereum.enable();
+         // Acccounts now exposed
+         return web3;
+       } catch (error) {
+         console.error(error);
+       }
+     }
+     // Legacy dapp browsers...
+     else if (window.web3) {
+       // Use Mist/MetaMask's provider.
+       const web3 = new Web3(window.web3.currentProvider);
+       console.log('Injected web3 detected.');
+       return web3;
+     }
+     // Fallback to localhost; use dev console port by default...
+     else {
+       const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+       const web3 = new Web3(provider);
+       console.log('No web3 instance injected, using Local web3.');
+       return web3;
+     }
+}
+
+
+async function checkBalance() {
+  const contract = new window.web3.eth.Contract(ERC20_ABI, ERC20_ADDRESS)
+  const account = (await window.web3.eth.getAccounts())[0]
+  const balance = await contract.methods.balanceOf(account).call()
+  document.getElementById('balance').innerHTML = balance
+}
+
+async function transfer() {
+  const recipient = document.getElementById('recipient').value;
+  const amount = document.getElementById('amount').value;
+  const account = (await window.web3.eth.getAccounts())[0]
+  const contract = new window.web3.eth.Contract(ERC20_ABI, ERC20_ADDRESS)
+  await contract.methods.transfer(recipient, amount).send({
+    from: account,
+    value: TRANSFER_FEE
+  })
+}
+
+window.addEventListener("load", () => {
+  connect().then((web3) => {window.web3 = web3});
+  document.getElementById('checkBalance').addEventListener('click', () => {checkBalance().then()})
+  document.getElementById('transfer').addEventListener('click', () => {transfer().then()})
+  // console.log(window.getElementById("checkBalance"))
+  // window.getElementById("transfer").addEventListener('click', () => {transfer().then()})
+})
